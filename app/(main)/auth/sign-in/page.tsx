f30e237789353cd39428bc13aec2dd1d665669a4
dberos <dberos.dev@gmail.com>
2025-03-22 17:39:03 +0200

    quick fix

--- app/(main)/auth/sign-in/page.tsx
+++ app/(main)/auth/sign-in/page.tsx
@@ -1,6 +1,8 @@
+import { headers } from "next/headers";
 import SignIn from "../../_components/sign-in";
 
 export default async function SignInPage() {
+    await headers();
     return (
         <main className="flex h-screen flex-col items-center justify-between">
             <div className="mt-20 h-[10vh] md:h-[20vh] 2k:h-[30vh] w-0.5 bg-muted-foreground dark:bg-muted" />
