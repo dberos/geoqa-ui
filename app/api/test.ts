20d96e4302a0f8a15a8a47433ba667dcee59df8c
dberos <dberos.dev@gmail.com>
2025-04-04 18:26:03 +0300

    remove commented out code

--- app/api/test.ts
+++ app/api/test.ts
@@ -1,58 +1,10 @@
-import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { helloSchema } from "@/schemas";
 
 // Rename file to index.ts to have root route
 // Create more files in this folder to have nested routes
 
 const app = new Hono()
-    // .post(
-    //     '/login', 
-    //     zValidator("json", loginSchema), 
-    //     async (c) => {
-    //         try {
-    //             const { email, password } = c.req.valid("json");
-
-    //             const [newUser] = await db
-    //             .insert(usersTable)
-    //             .values({ email, password })
-    //             .returning();
-    //             console.log(newUser);
-
-    //             const accessToken = await createJWT(email, '10s') || "";
-    //             const refreshToken = await createJWT(email, '1h') || "";
-
-    //             setCookie(c, 'accessToken', accessToken, {
-    //                 httpOnly: true,
-    //                 secure: process.env.NODE_ENV === 'production',
-    //                 sameSite: 'lax',
-    //                 maxAge: 60 * 60 * 24,
-    //                 path: '/',
-    //             });
-
-    //             setCookie(c, 'refreshToken', refreshToken, {
-    //                 httpOnly: true,
-    //                 secure: process.env.NODE_ENV === 'production',
-    //                 sameSite: 'lax',
-    //                 maxAge: 60 * 60 * 24,
-    //                 path: '/',
-    //             });
-
-
-    //             return c.json({
-    //                 email,
-    //                 password
-    //             })
-
-
-    //         }
-    //         catch (error) {
-    //             console.error(error);
-    //             return c.json({ error: "Error" }, { status: 500 });
-    //         }
-            
-    // })
     .get('/gis', async (c) => {
         const response = await fetch(process.env.NLQ_API_URL!, {
             method: 'POST',
@@ -69,25 +21,6 @@ const app = new Hono()
     .get('/', sessionMiddleware, async (c) => {
         const session = c.get('session');
         return c.json({ session });
-    })
-    .get('/hello', (c) => {
-        return c.json({ message: "Hello World" });
-    })
-    .post(
-        '/hello', 
-        zValidator("json", helloSchema), 
-        async (c) => {
-            try {
-                const { message } = c.req.valid("json");
-                return c.json({ message })
-
-
-            }
-            catch (error) {
-                console.error(error);
-                return c.json({ error: "Error" }, { status: 500 });
-            }
-            
     })
 
 
