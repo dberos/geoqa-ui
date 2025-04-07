92cfe532e39305f9be9c97dc6a86d745a7e86dbb
dberos <dberos.dev@gmail.com>
2025-04-07 19:55:23 +0300

    refactor sign in code

--- app/api/auth/sign-in.ts
+++ app/api/auth/sign-in.ts
@@ -2,7 +2,7 @@ import { db } from "@/db";
 import { sessionsTable, tokensTable, usersTable } from "@/db/schema";
 import { createJWT, decodeJWT } from "@/lib/session";
 import { SignInSchema } from "@/schemas";
-import { OathEnum, OathUserType, SessionPayload } from "@/types";
+import { OathEnum, OathUserType, SessionPayload, UserType } from "@/types";
 import { zValidator } from "@hono/zod-validator";
 import { eq } from "drizzle-orm";
 import { Context, Hono } from "hono";
@@ -33,6 +33,86 @@ const app = new Hono()
         }
     )
 
+const deleteSessions = async (user: UserType, now: number) => {
+    // Delete all inactive user sessions
+    const sessions = await db
+    .select()
+    .from(sessionsTable)
+    .where(eq(sessionsTable.userId, user.id));
+
+    for (const session of sessions) {
+        // Get all tokens for the session
+        const tokens = await db
+        .select()
+        .from(tokensTable)
+        .where(eq(tokensTable.sessionId, session.id));
+
+        // Check if all tokens have expired
+        const allTokensExpired = tokens.every(token => token.exp < now);
+
+        if (allTokensExpired) {
+            // Delete the session if all its tokens are expired
+            await db
+            .delete(sessionsTable)
+            .where(eq(sessionsTable.id, session.id));
+        }
+    }
+}
+
+const createSession = async (user: UserType, now: number, c: Context) => {
+    // Create a new session
+    const [newSession] = await db
+    .insert(sessionsTable)
+    .values({ userId: user.id })
+    .returning();
+
+    // Create a jti for the refresh token
+    const jti = uuidv4();
+
+    // Create the session JWTs
+    const accessPayload: SessionPayload = {
+        id: user.id,
+        name: user.name,
+        avatarUrl: user.avatarUrl
+    };
+    const refreshPayload: SessionPayload = {
+        sessionId: newSession.id,
+    }
+
+    const newAccessToken = await createJWT(accessPayload, '10s') || "";
+    const newRefreshToken = await createJWT(refreshPayload, '1h', jti) || "";
+
+    // Get the exact exp of the token to be stored
+    const decodedRefreshToken = await decodeJWT(newRefreshToken);
+    const exp = decodedRefreshToken?.exp ||  now + 3600;
+
+    // Insert the token's fields to the tokens table
+    await db
+    .insert(tokensTable)
+    .values({
+        sessionId: newSession.id,
+        jti,
+        exp,
+    });
+
+    // Set the cookies
+    setCookie(c, 'accessToken', newAccessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        maxAge: 60 * 60 * 24,
+        path: '/',
+    });
+
+    setCookie(c, 'refreshToken', newRefreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        maxAge: 60 * 60 * 24,
+        path: '/',
+    });
+}
+
 const signInWithGitHub = async (user: OathUserType, c: Context) => {
     try {
         const [existingUser] = await db
@@ -52,83 +132,14 @@ const signInWithGitHub = async (user: OathUserType, c: Context) => {
             .where(eq(usersTable.githubId, user.id))
             .returning();
 
-            // Delete all inactive user sessions
-            const sessions = await db
-            .select()
-            .from(sessionsTable)
-            .where(eq(sessionsTable.userId, updatedUser.id));
-
+            // Get current time
             const now = Math.floor(Date.now() / 1000);
 
-            for (const session of sessions) {
-                // Get all tokens for the session
-                const tokens = await db
-                .select()
-                .from(tokensTable)
-                .where(eq(tokensTable.sessionId, session.id));
-
-                // Check if all tokens have expired
-                const allTokensExpired = tokens.every(token => token.exp < now);
-
-                if (allTokensExpired) {
-                    // Delete the session if all its tokens are expired
-                    await db
-                    .delete(sessionsTable)
-                    .where(eq(sessionsTable.id, session.id));
-                }
-            }
+            // Delete all inactive user sessions
+            await deleteSessions(updatedUser, now);
 
             // Create a new session
-            const [newSession] = await db
-            .insert(sessionsTable)
-            .values({ userId: updatedUser.id })
-            .returning();
-
-            // Create a jti for the refresh token
-            const jti = uuidv4();
-
-            // Create the session JWTs
-            const accessPayload: SessionPayload = {
-                id: updatedUser.id,
-                name: updatedUser.name,
-                avatarUrl: updatedUser.avatarUrl
-            };
-            const refreshPayload: SessionPayload = {
-                sessionId: newSession.id,
-            }
-
-            const newAccessToken = await createJWT(accessPayload, '10s') || "";
-            const newRefreshToken = await createJWT(refreshPayload, '1h', jti) || "";
-
-            // Get the exact exp of the token to be stored
-            const decodedRefreshToken = await decodeJWT(newRefreshToken);
-            const exp = decodedRefreshToken?.exp ||  now + 3600;
-
-            // Insert the token's fields to the tokens table
-            await db
-            .insert(tokensTable)
-            .values({
-                sessionId: newSession.id,
-                jti,
-                exp,
-            });
-
-            // Set the cookies
-            setCookie(c, 'accessToken', newAccessToken, {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: 'lax',
-                maxAge: 60 * 60 * 24,
-                path: '/',
-            });
-
-            setCookie(c, 'refreshToken', newRefreshToken, {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: 'lax',
-                maxAge: 60 * 60 * 24,
-                path: '/',
-            });
+            await createSession(updatedUser, now, c);
         }
         else {
             // If user doesn't exist, create a new user
@@ -139,83 +150,14 @@ const signInWithGitHub = async (user: OathUserType, c: Context) => {
             })
             .returning();
 
-            // Delete all inactive user sessions
-            const sessions = await db
-            .select()
-            .from(sessionsTable)
-            .where(eq(sessionsTable.userId, newUser.id));
-
+            // Get current time
             const now = Math.floor(Date.now() / 1000);
 
-            for (const session of sessions) {
-                // Get all tokens for the session
-                const tokens = await db
-                .select()
-                .from(tokensTable)
-                .where(eq(tokensTable.sessionId, session.id));
-
-                // Check if all tokens have expired
-                const allTokensExpired = tokens.every(token => token.exp < now);
-
-                if (allTokensExpired) {
-                    // Delete the session if all its tokens are expired
-                    await db
-                    .delete(sessionsTable)
-                    .where(eq(sessionsTable.id, session.id));
-                }
-            }
+            // Delete all inactive user sessions
+            await deleteSessions(newUser, now)
 
             // Create a new session
-            const [newSession] = await db
-            .insert(sessionsTable)
-            .values({ userId: newUser.id })
-            .returning();
-
-            // Create a jti for the refresh token
-            const jti = uuidv4();
-
-            // Create the session JWTs
-            const accessPayload: SessionPayload = {
-                id: newUser.id,
-                name: newUser.name,
-                avatarUrl: newUser.avatarUrl
-            };
-            const refreshPayload: SessionPayload = {
-                sessionId: newSession.id,
-            }
-
-            const newAccessToken = await createJWT(accessPayload, '10s') || "";
-            const newRefreshToken = await createJWT(refreshPayload, '1h', jti) || "";
-
-            // Get the exact exp of the token to be stored
-            const decodedRefreshToken = await decodeJWT(newRefreshToken);
-            const exp = decodedRefreshToken?.exp ||  now + 3600;
-
-            // Insert the token's fields to the tokens table
-            await db
-            .insert(tokensTable)
-            .values({
-                sessionId: newSession.id,
-                jti,
-                exp,
-            });
-
-            // Set the cookies
-            setCookie(c, 'accessToken', newAccessToken, {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: 'lax',
-                maxAge: 60 * 60 * 24,
-                path: '/',
-            });
-
-            setCookie(c, 'refreshToken', newRefreshToken, {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: 'lax',
-                maxAge: 60 * 60 * 24,
-                path: '/',
-            });
+            await createSession(newUser, now, c);
         }
         return c.json({ success: true });
     }
@@ -244,83 +186,14 @@ const signInWithGoogle = async (user: OathUserType, c: Context) => {
             .where(eq(usersTable.googleId, user.id))
             .returning();
 
-            // Delete all inactive user sessions
-            const sessions = await db
-            .select()
-            .from(sessionsTable)
-            .where(eq(sessionsTable.userId, updatedUser.id));
-
+            // Get current time
             const now = Math.floor(Date.now() / 1000);
 
-            for (const session of sessions) {
-                // Get all tokens for the session
-                const tokens = await db
-                .select()
-                .from(tokensTable)
-                .where(eq(tokensTable.sessionId, session.id));
-
-                // Check if all tokens have expired
-                const allTokensExpired = tokens.every(token => token.exp < now);
-
-                if (allTokensExpired) {
-                    // Delete the session if all its tokens are expired
-                    await db
-                    .delete(sessionsTable)
-                    .where(eq(sessionsTable.id, session.id));
-                }
-            }
+            // Delete all inactive user sessions
+            await deleteSessions(updatedUser, now)
 
             // Create a new session
-            const [newSession] = await db
-            .insert(sessionsTable)
-            .values({ userId: updatedUser.id })
-            .returning();
-
-            // Create a jti for the refresh token
-            const jti = uuidv4();
-
-            // Create the session JWTs
-            const accessPayload: SessionPayload = {
-                id: updatedUser.id,
-                name: updatedUser.name,
-                avatarUrl: updatedUser.avatarUrl
-            };
-            const refreshPayload: SessionPayload = {
-                sessionId: newSession.id,
-            }
-
-            const newAccessToken = await createJWT(accessPayload, '10s') || "";
-            const newRefreshToken = await createJWT(refreshPayload, '1h', jti) || "";
-
-            // Get the exact exp of the token to be stored
-            const decodedRefreshToken = await decodeJWT(newRefreshToken);
-            const exp = decodedRefreshToken?.exp ||  now + 3600;
-
-            // Insert the token's fields to the tokens table
-            await db
-            .insert(tokensTable)
-            .values({
-                sessionId: newSession.id,
-                jti,
-                exp,
-            });
-
-            // Set the cookies
-            setCookie(c, 'accessToken', newAccessToken, {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: 'lax',
-                maxAge: 60 * 60 * 24,
-                path: '/',
-            });
-
-            setCookie(c, 'refreshToken', newRefreshToken, {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: 'lax',
-                maxAge: 60 * 60 * 24,
-                path: '/',
-            });
+            await createSession(updatedUser, now, c);
         }
         else {
             // If user doesn't exist, create a new user
@@ -331,83 +204,14 @@ const signInWithGoogle = async (user: OathUserType, c: Context) => {
             })
             .returning();
 
-            // Delete all inactive user sessions
-            const sessions = await db
-            .select()
-            .from(sessionsTable)
-            .where(eq(sessionsTable.userId, newUser.id));
-
+            // Get current time
             const now = Math.floor(Date.now() / 1000);
 
-            for (const session of sessions) {
-                // Get all tokens for the session
-                const tokens = await db
-                .select()
-                .from(tokensTable)
-                .where(eq(tokensTable.sessionId, session.id));
-
-                // Check if all tokens have expired
-                const allTokensExpired = tokens.every(token => token.exp < now);
-
-                if (allTokensExpired) {
-                    // Delete the session if all its tokens are expired
-                    await db
-                    .delete(sessionsTable)
-                    .where(eq(sessionsTable.id, session.id));
-                }
-            }
+            // Delete all inactive user sessions
+            await deleteSessions(newUser, now);
 
             // Create a new session
-            const [newSession] = await db
-            .insert(sessionsTable)
-            .values({ userId: newUser.id })
-            .returning();
-
-            // Create a jti for the refresh token
-            const jti = uuidv4();
-
-            // Create the session JWTs
-            const accessPayload: SessionPayload = {
-                id: newUser.id,
-                name: newUser.name,
-                avatarUrl: newUser.avatarUrl
-            };
-            const refreshPayload: SessionPayload = {
-                sessionId: newSession.id,
-            }
-
-            const newAccessToken = await createJWT(accessPayload, '10s') || "";
-            const newRefreshToken = await createJWT(refreshPayload, '1h', jti) || "";
-
-            // Get the exact exp of the token to be stored
-            const decodedRefreshToken = await decodeJWT(newRefreshToken);
-            const exp = decodedRefreshToken?.exp ||  now + 3600;
-
-            // Insert the token's fields to the tokens table
-            await db
-            .insert(tokensTable)
-            .values({
-                sessionId: newSession.id,
-                jti,
-                exp,
-            });
-
-            // Set the cookies
-            setCookie(c, 'accessToken', newAccessToken, {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: 'lax',
-                maxAge: 60 * 60 * 24,
-                path: '/',
-            });
-
-            setCookie(c, 'refreshToken', newRefreshToken, {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: 'lax',
-                maxAge: 60 * 60 * 24,
-                path: '/',
-            });
+            await createSession(newUser, now, c);
         }
         return c.json({ success: true });
     }
