bbe7e2fc2d4fdef0c7428f910ca126b0331b71bf
dberos <dberos.dev@gmail.com>
2025-07-22 17:00:56 +0300

    quick fix

--- app/(main)/_components/hero.tsx
+++ app/(main)/_components/hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
                 GeoQA
             </h1>
             <h2 className="text-3xl lg:text-4xl 2k:text-5xl font-semibold text-white max-w-xs md:max-w-md lg:max-w-xl 2k:max-w-2xl text-center">
-                AI powered QnA engine for geospatial data
+                AI powered QA engine for geospatial data
             </h2>
             <Button className="cursor-pointer 4k:py-8 4k:px-6 4k:rounded-xl">
                 <Link href="/dashboard" className="flex items-center justify-center gap-x-2 4k:text-xl">
